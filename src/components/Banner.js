3e878875c0eeec3c06a51d96616badc064c79f9b
Daniele410 <miraglia.daniele410@gmail.com>
2023-05-23 11:21:48 +0200

    add shoppingList

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,33 +1,33 @@
 import '../styles/Banner.css'
 import logo from '../assets/logo.png'
 function Banner() {
-    //bonne façon
-    const title = 'La maison jungle'
-    return (
-        <div className='lmj-banner'>
-            <img src={logo} alt='La maison jungle' className='lmj-logo' />
-            <h1 className='lmj-title'>{title}</h1>
-        </div>
-    )
+  //bonne façon
+  const title = 'La maison jungle'
+  return (
+    <div className='lmj-banner'>
+      <img src={logo} alt='La maison jungle' className='lmj-logo' />
+      <h1 className='lmj-title'>{title}</h1>
+    </div>
+  )
 }
 
 export default Banner
-  
-  
-  
-  
+
+
+
+
   // premier faison
-      /*const title = "La maison jungle"
-    return (<h1>{title}</h1>) */
+/*const title = "La maison jungle"
+return (<h1>{title}</h1>) */
 
     // deuxième façon
-        /* return (
-            <div style={{
-              color:'black',
-              textAlign: 'right',
-              padding: 32,
-              borderBottom: 'solid 3px black'
-            }}>
-                <h1>La maison jungle</h1>
-            </div> 
-        )*/
+/* return (
+    <div style={{
+      color:'black',
+      textAlign: 'right',
+      padding: 32,
+      borderBottom: 'solid 3px black'
+    }}>
+        <h1>La maison jungle</h1>
+    </div> 
+)*/
