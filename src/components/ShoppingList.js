0c9bd380bfbaba227bc48c0cd29414c66974c66f
Daniele410 <miraglia.daniele410@gmail.com>
2023-05-26 15:44:05 +0200

    add categories component

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,10 +1,12 @@
 import { plantList } from '../datas/plantList'
 import PlantItem from './PlantItem'
+import Categories from './Categories'
+import { useState } from 'react'
 import '../styles/ShoppingList.css'
 
 function ShoppingList({ cart, updateCart }) {
 	// Petite précision : categories nous vient de la partie précédente pour récupérer toutes les catégories uniques de plantes.
-
+	const [activeCategory, setActiveCategory] = useState('')
 	const categories = plantList.reduce(
 		(acc, plant) =>
 			acc.includes(plant.category) ? acc : acc.concat(plant.category),
@@ -28,24 +30,27 @@ function ShoppingList({ cart, updateCart }) {
 
 	return (
 		<div className='lmj-shopping-list'>
-			<ul>
-				{categories.map((cat) => (
-					<li key={cat}>{cat}</li>
-				))}
-			</ul>
+			<Categories
+				categories={categories}
+				setActiveCategory={setActiveCategory}
+				activeCategory={activeCategory}
+			/>
+
 			<ul className='lmj-plant-list'>
-				{plantList.map(({ id, cover, name, water, light, price }) => (
-					<div key={id}>
-						<PlantItem
-							cover={cover}
-							name={name}
-							water={water}
-							light={light}
-							price={price}
-						/>
-						<button onClick={() => addToCart(name, price)}>Ajouter</button>
-					</div>
-				))}
+				{plantList.map(({ id, cover, name, water, light, price, category }) =>
+					!activeCategory || activeCategory === category ? (
+							<div key={id}>
+								<PlantItem
+									cover={cover}
+									name={name}
+									water={water}
+									light={light}
+									price={price}
+								/>
+								<button onClick={() => addToCart(name, price)}>Ajouter</button>
+							</div>
+						) : null
+				)}
 			</ul>
 		</div>
 	)
